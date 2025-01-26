5294e3726004278187aee56b3568267e138f6487
RufaAnt700 <alexbrim04@gmail.com>
2025-01-25 16:25:32 -0800

    Improved Enemy Sight
    I added a trigger, restricting the sight of the fish. Terrain and my personal scene was also modified

--- DeepSea/Assets/Scripts/RufaScripts/ChasingFish.cs
+++ DeepSea/Assets/Scripts/RufaScripts/ChasingFish.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class AnnoyingFish : MonoBehaviour
@@ -10,7 +11,7 @@ public class AnnoyingFish : MonoBehaviour
     public float _Speed = 2f;
     public Vector3 _Offset = new Vector3(0f, 1f, 0f);
 
-    //private bool _SeesPlayer = false;
+    private bool _SeesPlayer = false;
 
 
     // Start is called before the first frame update
@@ -23,7 +24,7 @@ public class AnnoyingFish : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (CanSeePlayer())
+        if (CanSeePlayer() && _SeesPlayer)
         {
             Vector3 _Direction = (_PlayerTransform.position + _Offset - transform.position).normalized;
             transform.forward = _Direction;
@@ -38,23 +39,35 @@ public class AnnoyingFish : MonoBehaviour
             Debug.Log("Collided with Player");
         }
     }
-    /*
-    private void OnTriggerStay(Collider other)
+    
+    private void OnTriggerEnter(Collider _Entity)
     {
-        if (other.gameObject == _PlayerTransform)
+        if (_Entity.transform == _PlayerTransform)
         {
-            _SeesPlayer = SeesPlayer();
+            Debug.Log("Found player");
+
+            _SeesPlayer = true;
         }
     }
-    */
+
+    private void OnTriggerExit(Collider _Entity)
+    {
+        if (_Entity.transform == _PlayerTransform)
+        {
+            Debug.Log("Lost player");
+
+            _SeesPlayer = false;
+        }
+    }
+    
     private bool CanSeePlayer()
     {
         RaycastHit[] _Objects = Physics.RaycastAll(transform.position, _PlayerTransform.position + _Offset - transform.position);
+        Debug.Log(_Objects.Length);
         
         for (int i = 0; i < _Objects.Length; i++)
         {
-            Debug.Log(_Objects[i]);
-            if (_Objects[i].collider.gameObject.tag == "Obstruction")
+            if (_Objects[i].collider.gameObject.CompareTag("Obstruction"))
                 return false;
         }
         return true;
